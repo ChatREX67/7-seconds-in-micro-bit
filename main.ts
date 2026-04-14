2522c180fa35526011a97bc33a76ff00cbc14820
ChatREX67 <Rgraziano@summitviewvalley.com>
2026-04-14 15:24:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let start = 0
+let elapsed = 0
+let score = 0
+input.onButtonPressed(Button.A, function () {
+    start = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - start
+    score = Math.abs(elapsed - 7000)
+})
